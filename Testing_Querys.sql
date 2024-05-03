936f2489e4366d538c7bdae9aa3b3106a4add7a5
Marss616 <67536529+Marss616@users.noreply.github.com>
2024-05-03 18:13:20 +1000

    Update

--- Testing_Querys.sql
+++ Testing_Querys.sql
@@ -1,4 +1,6 @@
 --30/4/24
+
+
 --1
 select V.venueName, E.eventName
 from Events E, EventVenues EV, Venues V
@@ -41,6 +43,8 @@ where eventVenues.bookingDate = 'JUN-2018';
 
 
 --2/5/24
+
+
 --1
 SELECT * from tab;
 SELECT * from CLIENTS;
@@ -73,3 +77,52 @@ ORDER BY VENUECAPACITYREQUIRED;
 SELECT MIN(Salary) as MIN_SAL, MAX(Salary) as MAX_SAL, AVG(Salary) as AVG_SAL
 FROM EMPLOYEE;
 
+
+--3/5/24
+
+--1
+SELECT * FROM TAB;
+--2
+DROP TABLE DEPARTMENT;
+--3
+DROP TABLE EMPLOYEE;
+--4
+PURGE RECYCLEBIN;
+--5
+SELECT * FROM TAB;
+SELECT * FROM DEPARTMENT;
+SELECT * FROM EMPLOYEE;
+DROP TABLE DEPARTMENT;
+DROP TABLE EMPLOYEE;
+PURGE RECYCLEBIN;
+--6
+
+CREATE TABLE EMPLOYEE (
+    Employee_No VARCHAR(10),
+    First_Name VARCHAR(50),
+    Last_Name VARCHAR(50),
+    Dept_Number VARCHAR(10),
+    Salary INT
+);
+
+INSERT INTO EMPLOYEE (Employee_No, First_Name, Last_Name, Dept_Number, Salary) VALUES ('E1', 'Mandy', 'Smith', 'D1', 50000);
+INSERT INTO EMPLOYEE (Employee_No, First_Name, Last_Name, Dept_Number, Salary) VALUES ('E2', 'Daniel', 'Hodges', 'D2', 45000);
+INSERT INTO EMPLOYEE (Employee_No, First_Name, Last_Name, Dept_Number, Salary) VALUES ('E3', 'Shaskia', 'Ramanthan', 'D2', 58000);
+INSERT INTO EMPLOYEE (Employee_No, First_Name, Last_Name, Dept_Number, Salary) VALUES ('E4', 'Graham', 'Burke', 'D1', 64000);
+INSERT INTO EMPLOYEE (Employee_No, First_Name, Last_Name, Dept_Number, Salary) VALUES ('E5', 'Annie', 'Nguyen', 'D1', 60000);
+
+--7
+
+CREATE TABLE DEPARTMENT (
+    Dept_Number VARCHAR(10),
+    Dept_Name VARCHAR(50),
+    Location VARCHAR(50),
+    Mail_Number INT
+);
+
+--Add data to DEPARTMENT Table
+
+INSERT INTO DEPARTMENT (Dept_Number, Dept_Name, Location, Mail_Number) VALUES ('D1', 'Computer Science', 'Bundoora', 39);
+INSERT INTO DEPARTMENT (Dept_Number, Dept_Name, Location, Mail_Number) VALUES ('D2', 'Information Science', 'Bendigo', 30);
+INSERT INTO DEPARTMENT (Dept_Number, Dept_Name, Location, Mail_Number) VALUES ('D3', 'Physics', 'Bundoora', 37);
+INSERT INTO DEPARTMENT (Dept_Number, Dept_Name, Location, Mail_Number) VALUES ('D4', 'Chemistry', 'Bendigo', 35);
